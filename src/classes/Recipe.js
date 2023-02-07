77ff49227603883932d9db8ec6e9fb9f36cc5052
Jason Kirchman <kirchman.jason@icloud.com>
2023-02-06 18:07:31 -0600

    feat: Add propeties to recipe class and associated tests

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,6 +1,13 @@
-class Recipe {
-  constructor() {
+import Ingredient from './Ingredient';
 
+class Recipe {
+  constructor(recipeInfo) {
+    this.id = recipeInfo.id;
+    this.image = recipeInfo.image;
+    this.ingredients = recipeInfo.ingredients.map(ingredient => new Ingredient(ingredient));
+    this.insructions = recipeInfo.insructions;
+    this.name = recipeInfo.name;
+    this.tags = recipeInfo.tags;
   }
 }
 
