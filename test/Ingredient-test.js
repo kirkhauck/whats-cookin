9e7e5ad952b27d40be99755f1424780c8362593d
Kirk Hauck <kirkihauck@gmail.com>
2023-02-06 17:18:45 -0700

    fix: Test ingredient class with beforeEach

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -6,14 +6,14 @@ describe('Ingredient', () => {
 
   beforeEach(() => {
     console.log("cabbage")
-    sampleIngredients =
-      {
-        "id": 20081,
-        "name": "wheat flour",
-        "estimatedCostInCents": 142
-      }
+    sampleIngredients = [
+    {
+      "id": 20081,
+      "name": "wheat flour",
+      "estimatedCostInCents": 142
+    }];
     
-    ingredient = new Ingredient();
+    ingredient = new Ingredient(sampleIngredients[0]);
   });
 
   it('Should be a function', () => {
@@ -21,54 +21,18 @@ describe('Ingredient', () => {
   });
 
   it('Should be an instance of Ingredient', () => {
-    // const sampleIngredients =
-    // {
-    //   "id": 20081,
-    //   "name": "wheat flour",
-    //   "estimatedCostInCents": 142
-    // }
-  
-    // const ingredient = new Ingredient(sampleIngredients);
-
     expect(ingredient).to.be.an.instanceOf(Ingredient);
   });
 
   it('Should have an id', () => {
-    // const sampleIngredients =
-    // {
-    //   "id": 20081,
-    //   "name": "wheat flour",
-    //   "estimatedCostInCents": 142
-    // }
-  
-    // const ingredient = new Ingredient(sampleIngredients);
-
     expect(ingredient.id).to.equal(20081);
   });
 
   it('Should have a name', () => {
-    // const sampleIngredients =
-    // {
-    //   "id": 20081,
-    //   "name": "wheat flour",
-    //   "estimatedCostInCents": 142
-    // }
-  
-    // const ingredient = new Ingredient(sampleIngredients);
-
     expect(ingredient.name).to.equal('wheat flour');
   });
 
   it('Should have a estimated cost', () => {
-    // const sampleIngredients =
-    // {
-    //   "id": 20081,
-    //   "name": "wheat flour",
-    //   "estimatedCostInCents": 142
-    // }
-  
-    // const ingredient = new Ingredient(sampleIngredients);
-
     expect(ingredient.ingredientCost).to.equal(142);
   });
 });
