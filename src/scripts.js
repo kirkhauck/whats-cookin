b2239d9a60ce6badf7e40591894392f694c48684
Katherine Blaine <blaine.katherine12@gmail.com>
2023-02-07 18:36:34 -0700

    feat: add filter by tag list and button

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,9 @@ import recipeData from './data/recipes';
 import ingredientsData from './data/ingredients';
 
 const recipeSection = document.getElementById('recipes-section');
+const recipeRepository = new RecipeRepository(recipeData, ingredientsData);
+const tagSection = document.querySelector('.tags');
+const filterByTagButton = document.getElementById('tagButton');
 
 
 console.log('Hello world');
@@ -14,10 +17,19 @@ console.log('Hello world');
 // User should be able to view a list of all recipes.
 
 window.addEventListener('load', () => {
-    const recipeRepository = new RecipeRepository(recipeData, ingredientsData);
     recipeRepository.recipes.forEach(recipe => {
         recipeSection.innerHTML += `
         <figure><img src="${recipe.image}"><figcaption>${recipe.name}</figcaption></figure>
         `
     })
 })
+
+window.addEventListener('load', () => {
+    const recipeTags = [...new Set(recipeRepository.recipes.flatMap(recipe => recipe.tags))].sort();
+    recipeTags.forEach(tag => {
+        tagSection.innerHTML += `
+        <option value="${tag}">${tag}</option>
+        `
+    })
+})
+
