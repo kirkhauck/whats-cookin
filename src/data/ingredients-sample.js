4cb977c0fb3be3386c2a2f23bc18ba6fca9ff854
Kara Anderson <kanderson8585@gmail.com>
2023-02-07 17:07:18 -0700

    feat: Create mock data and passed it to tests

--- src/data/ingredients-sample.js
+++ src/data/ingredients-sample.js
@@ -15,14 +15,24 @@ const ingredientsSampleData = [
       "estimatedCostInCents": 472
     },
     {
-      "id": 19335,
-      "name": "sucrose",
-      "estimatedCostInCents": 902
+      "id": 1009016,
+      "name": "apple cider",
+      "estimatedCostInCents": 468
     },
     {
-      "id": 19206,
-      "name": "instant vanilla pudding",
-      "estimatedCostInCents": 660
+      "id": 9003,
+      "name": "apple",
+      "estimatedCostInCents": 207
+    },
+    {
+      "id": 20027,
+      "name": "corn starch",
+      "estimatedCostInCents": 236
+    },
+    {
+      "id": 1002030,
+      "name": "black pepper",
+      "estimatedCostInCents": 441
     },
     {
       "id": 19334,
@@ -30,8 +40,35 @@ const ingredientsSampleData = [
       "estimatedCostInCents": 559
     },
     {
-      "id": 2047,
-      "name": "salt",
-      "estimatedCostInCents": 280
+      "id": 1001,
+      "name": "butter",
+      "estimatedCostInCents": 618
+    },
+    {
+      "id": 18371,
+      "name": "baking powder",
+      "estimatedCostInCents": 346
+    },
+    {
+      "id": 9040,
+      "name": "ripe banana",
+      "estimatedCostInCents": 331
+    },
+    {
+      "id": 2048,
+      "name": "apple cider vinegar",
+      "estimatedCostInCents": 532
+    },
+    {
+      "id": 20090,
+      "name": "brown rice flour",
+      "estimatedCostInCents": 667
+    },
+    {
+      "id": 19350,
+      "name": "corn syrup",
+      "estimatedCostInCents": 441
     }
   ]
+
+  export default ingredientsSampleData;
