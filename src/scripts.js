1d3eacf44260bc3e143d4810af94bca34124a6fa
Katherine Blaine <blaine.katherine12@gmail.com>
2023-02-07 19:42:41 -0700

    feat: complete functionality for filter by tag form and button

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@ import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/magnify.svg'
+
 import RecipeRepository from './classes/RecipeRepository';
 import recipeData from './data/recipes';
 import ingredientsData from './data/ingredients';
@@ -14,38 +15,40 @@ const filterByTagButton = document.getElementById('tagButton');
 const searchInputName = document.getElementById('search-input-name');
 const searchButtonName = document.getElementById('search-button-name');
 
-
 window.addEventListener('load', () => {
     recipeRepository = new RecipeRepository(recipeData, ingredientsData);
     currentRecipes = recipeRepository.recipes;
     refreshRecipes();
-})
+});
+
+window.addEventListener('load', () => {
+    const recipeTags = [...new Set(recipeRepository.recipes.flatMap(recipe => recipe.tags))].sort();
+    recipeTags.forEach(tag => {
+      tagSection.innerHTML += `
+          <option value="${tag}">${tag}</option>`;
+    });
+});
 
 searchButtonName.addEventListener('click', (event) => {
     event.preventDefault();
-    if(searchInputName.value === ''){
+    if(searchInputName.value === '') {
         currentRecipes = recipeRepository.recipes
-    }else{
+    } else {
         currentRecipes = recipeRepository.filterByName(searchInputName.value);
     }
     refreshRecipes();
-})
+});
 
-
-window.addEventListener('load', () => {
-  const recipeTags = [...new Set(recipeRepository.recipes.flatMap(recipe => recipe.tags))].sort();
-  recipeTags.forEach(tag => {
-    tagSection.innerHTML += `
-        <option value="${tag}">${tag}</option>
-        `
-      });
-    });
-    
-  const refreshRecipes = () => {
+filterByTagButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    currentRecipes = recipeRepository.filterByTag(tagSection.value);
+    refreshRecipes();
+});
+  
+const refreshRecipes = () => {
     recipeSection.innerHTML = '';
     currentRecipes.forEach(recipe => {
-      recipeSection.innerHTML += `
-          <figure><img src="${recipe.image}"><figcaption>${recipe.name}</figcaption></figure>
-      `;
-  });
-}  
+        recipeSection.innerHTML += `
+        <figure><img src="${recipe.image}"><figcaption>${recipe.name}</figcaption></figure>`;
+    });
+};
