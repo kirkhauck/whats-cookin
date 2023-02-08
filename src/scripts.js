e8d7eae3d58e026621d993d0ccc388e65177c1b0
Kirk Hauck <kirkihauck@gmail.com>
2023-02-07 20:29:11 -0700

    fix: Move MicroModal.init into refreshRecipes

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,8 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 import MicroModal from 'micromodal';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/magnify.svg'
-
 import RecipeRepository from './classes/RecipeRepository';
 import recipeData from './data/recipes';
 import ingredientsData from './data/ingredients';
@@ -19,44 +16,44 @@ const searchInputName = document.getElementById('search-input-name');
 const searchButtonName = document.getElementById('search-button-name');
 
 window.addEventListener('load', () => {
-    recipeRepository = new RecipeRepository(recipeData, ingredientsData);
-    currentRecipes = recipeRepository.recipes;
-    refreshRecipes();
-    MicroModal.init({
-      onShow: modal => console.info(`${modal.id} is shown`), // [1]
-      onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-      openTrigger: 'data-custom-open'
-    });
+  recipeRepository = new RecipeRepository(recipeData, ingredientsData);
+  currentRecipes = recipeRepository.recipes;
+  refreshRecipes();
 });
 
 window.addEventListener('load', () => {
-    const recipeTags = [...new Set(recipeRepository.recipes.flatMap(recipe => recipe.tags))].sort();
-    recipeTags.forEach(tag => {
-      tagSection.innerHTML += `
-          <option value="${tag}">${tag}</option>`;
-    });
+  const recipeTags = [...new Set(recipeRepository.recipes.flatMap(recipe => recipe.tags))].sort();
+  recipeTags.forEach(tag => {
+    tagSection.innerHTML += `
+      <option value="${tag}">${tag}</option>`;
+  });
 });
 
 searchButtonName.addEventListener('click', (event) => {
-    event.preventDefault();
-    if(searchInputName.value === '') {
-        currentRecipes = recipeRepository.recipes
-    } else {
-        currentRecipes = recipeRepository.filterByName(searchInputName.value);
-    }
-    refreshRecipes();
+  event.preventDefault();
+  if(searchInputName.value === '') {
+      currentRecipes = recipeRepository.recipes
+  } else {
+      currentRecipes = recipeRepository.filterByName(searchInputName.value);
+  }
+  refreshRecipes();
 });
 
 filterByTagButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    currentRecipes = recipeRepository.filterByTag(tagSection.value);
-    refreshRecipes();
+  event.preventDefault();
+  currentRecipes = recipeRepository.filterByTag(tagSection.value);
+  refreshRecipes();
 });
   
 const refreshRecipes = () => {
-    recipeSection.innerHTML = '';
-    currentRecipes.forEach(recipe => {
-        recipeSection.innerHTML += `
-        <figure data-custom-open="modal-1"><img src="${recipe.image}"><figcaption>${recipe.name}</figcaption></figure>`;
-    });
+  recipeSection.innerHTML = '';
+  currentRecipes.forEach(recipe => {
+    recipeSection.innerHTML += `
+    <figure data-custom-open="modal-1"><img src="${recipe.image}"><figcaption>${recipe.name}</figcaption></figure>`;
+  });
+  MicroModal.init({
+    onShow: modal => console.info(`${modal.id} is shown`), // [1]
+    onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+    openTrigger: 'data-custom-open'
+  });
 };
