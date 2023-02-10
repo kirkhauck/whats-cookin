7bdc11255d10a2c3da0b1f6e9f67885138cce568
Jason Kirchman <kirchman.jason@icloud.com>
2023-02-09 21:52:56 -0600

    refac: Moved api calls to their own file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,15 @@
-const getUsers = () => {
-    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
-        .then(response => response.json())
-        .then(users => users.usersData)
-        .catch(err => console.log('epic fail: ', err))
+const userPromise = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users');
+const recipePromise = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes');
+const ingredientPromise = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients');
+let fetchPromises = [userPromise, recipePromise, ingredientPromise]
+
+const apiCalls = () => {
+    return Promise.all(fetchPromises)
+        .then(responses => {
+            const jsonPromises = responses.map(response => response.json())
+            return Promise.all(jsonPromises)
+                .then(allData => allData)
+        })
 }
 
-export default getUsers;
+export default apiCalls;
