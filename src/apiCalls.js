c339a6ba26e41d8dace05c93284d3fd69d871e50
Katherine Blaine <blaine.katherine12@gmail.com>
2023-02-10 13:37:37 -0700

    refac: add minor updates for network request refactor

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,4 @@
-// const userPromise = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users');
-// const recipePromise = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes');
-// const ingredientPromise = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients');
-// const fetchPromises = [userPromise, recipePromise, ingredientPromise]
 import handleError from "./scripts";
-
-// const apiCalls = () => {
-//     return Promise.all(fetchPromises)
-//         .then(responses => {
-//             const jsonPromises = responses.map(response => response.json())
-//             return Promise.all(jsonPromises)
-//                 .then(allData => allData)
-//         })
-// }
 
 const fetchApi = (url) => {
     return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${url}`)
@@ -30,7 +17,7 @@ const fetchAllData = () => {
         fetchApi('recipes'),
         fetchApi('ingredients'),
     ])
-    .catch((error) => console.log(error))
+    .catch((error) => handleError(error))
 }
 
 
