9c136796efcf68984aea453b1822feee16efad60
Kara Anderson <kanderson8585@gmail.com>
2023-02-13 13:19:07 -0700

    Refac: clean up test files

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -6,6 +6,7 @@ import ingredientsSampleData from '../src/data/ingredients-sample';
 
 describe('Recipe', () => {
   let recipeInstance;
+  
   beforeEach(() => {
     recipeInstance = new Recipe(recipeSampleData[0], ingredientsSampleData);
   });
@@ -58,22 +59,22 @@ describe('Recipe', () => {
       "appetizer",
       "antipasto",
       "hor d'oeuvre"
-    ]
+    ];
     expect(recipeInstance.tags).to.deep.equal(tags);
   });
 
   it('Should be able to return an array of all ingredient names', () => {
     expect(recipeInstance.getIngredientNames()[0]).to.equal("wheat flour");
-    expect(recipeInstance.getIngredientNames().length).to.equal(3)
+    expect(recipeInstance.getIngredientNames().length).to.equal(3);
   })
 
   it('Should be able to return an array of cost in cents per ingredient', () => {
-    expect(recipeInstance.getIngredientCost()[0]).to.equal(213)
-    expect(recipeInstance.getIngredientCost().length).to.equal(3)
+    expect(recipeInstance.getIngredientCost()[0]).to.equal('2.13');
+    expect(recipeInstance.getIngredientCost().length).to.equal(3);
   })
 
   it('Should return an array of the recipe instructions', () => {
-    expect(recipeInstance.getInstructions()[0]).to.equal("In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.")
-    expect(recipeInstance.getInstructions()[2]).to.equal("Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.")
-  })
-})
+    expect(recipeInstance.getInstructions()[0]).to.equal("In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.");
+    expect(recipeInstance.getInstructions()[2]).to.equal("Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.");
+  });
+});
